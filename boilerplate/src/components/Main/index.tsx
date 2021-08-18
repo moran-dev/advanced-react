028cc92556297d8bdb72eeb8aa91f731ecd7519b
Vitor Moran <vitormoran@hotmail.com>
2021-08-17 21:19:56 -0300

    JestStyledComponents

--- boilerplate/src/components/Main/index.tsx
+++ boilerplate/src/components/Main/index.tsx
@@ -1,8 +1,21 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um Átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS, Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/illustration.svg"
+        alt="Desenvolvedor de Frente para a tela com um código"
+      />
+    </S.Wrapper>
   )
 }
 
