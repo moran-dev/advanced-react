54754cb6bc18999bbbe43fa84d0a77ce6336634f
Vitor Moran <vitormoran@hotmail.com>
2021-08-19 15:49:04 -0300

    StoryBookAndPWAConfiguration

--- boilerplate/src/components/Main/index.tsx
+++ boilerplate/src/components/Main/index.tsx
@@ -1,18 +1,19 @@
 import * as S from './styles'
 
-const Main = () => {
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS, Styled Components'
+}) => {
   return (
     <S.Wrapper>
       <S.Logo
         src="/img/logo.svg"
         alt="Imagem de um Átomo e React Avançado escrito ao lado."
       />
-      <S.Title>React Avançado</S.Title>
-      <S.Description>
-        TypeScript, ReactJS, NextJS, Styled Components
-      </S.Description>
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
       <S.Illustration
-        src="/img/illustration.svg"
+        src="/img/hero-illustration.svg"
         alt="Desenvolvedor de Frente para a tela com um código"
       />
     </S.Wrapper>
